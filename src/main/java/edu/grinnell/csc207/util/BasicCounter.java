be2cb1342eef22634c185b4d8382f336b06f4374
Bell <belljake@williamson.cs.grinnell.edu>
2024-10-01 14:50:39 -0500

    Add BasicCounter, tests, and experiments.

---
+++ src/main/java/edu/grinnell/csc207/util/BasicCounter.java
@@ -0,0 +1,54 @@
+package edu.grinnell.csc207.util;
+
+/**
+ * Basic Counter.
+ * 
+ * @author Jake Bell
+ * @author Drew Fargo
+ */
+public class BasicCounter implements Counter {
+    /** Value counter should reset to. */
+    protected int start;
+    /** Current value of the counter. */
+    protected int count;
+
+    /**
+     * Initialize with count startCount.
+     * 
+     * @param startCount The initial value and reset value.
+     */
+    public BasicCounter(int startCount) {
+        this.start = startCount;
+        this.count = startCount;
+    } // BasicCounter(int)
+
+    /**
+     * Increments the counter by one.
+     */
+    public void increment() {
+        this.count++;
+    } // increment()
+
+    /**
+     * Resets the counter to the starting value.
+     */
+    public void reset() {
+        this.count = this.start;
+    } // reset()
+
+    /**
+     * Represents this counter as a string:
+     * puts the current count value in brackets.
+     */
+    public String toString() {
+        return "[" + this.count + "]";
+    } // toString()
+
+    /**
+     * Accessor for the counter's value.
+     * @return The counter's current value.
+     */
+    public int get() {
+        return this.count;
+    } // get()
+} // class BasicCounter
